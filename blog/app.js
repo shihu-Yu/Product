71cbbf91d0525b87db140bf6ea64b653af9b3edd
Yushihu <1832237525@qq.com>
2020-10-30 10:48:49 +0800

    create blog

---
+++ blog/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const swig = require('swig')
+const app = express()
+
+
+app.use(express.static('public'))
+swig.setDefaults({
+    // cache: 'memory',//如果需要缓存需要添加
+    cache: false
+})
+app.engine('html', swig.renderFile);
+app.set('views', './views')
+app.set('view engine', 'html')
+
+//路由模块化 分离开来
+app.use('/',require('./routes/index'))
+
+app.listen(3000,()=>{
+    console.log('server is running at http://127.0.0.1:3000')
+})
